997e06c2e616f8393dbc86dd61765d07b1d0b881
Jonathan Pope <jonathanmpope@gmail.com>
2022-08-29 15:31:53 -0600

    feat: merchant API all request with tests

--- spec/requests/api/v1/merchants_spec.rb
+++ spec/requests/api/v1/merchants_spec.rb
@@ -7,13 +7,38 @@ describe "Merchants API" do
     get '/api/v1/merchants'
 
     expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(merchants).to have_key(:data)
+    expect(merchants[:data].count).to eq(3)
+    expect(merchants[:data].class).to eq(Array)
+
+    merchants[:data].each do |merchant|
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id]).to be_a(String)
+
+      expect(merchant).to have_key(:type)
+      expect(merchant[:type]).to eq("merchant")
+
+      expect(merchant).to have_key(:attributes)
+
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes][:name]).to be_a(String)
+
+      expect(merchant[:attributes]).to_not have_key(:created_at)
+      expect(merchant[:attributes]).to_not have_key(:updated_at)
+    end
   end
 
-  xit "sends an empty list of merchants when none are present" do
-    create_list(:merchant, 3)
-
+  it "sends an empty list of merchants when none are present" do
     get '/api/v1/merchants'
 
     expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(merchants).to have_key(:data)
+    expect(merchants[:data].count).to eq(0)
   end
 end
