782b797fe3734c2dae7a051de6e1e61bfd2b88b8
Jonathan Pope <jonathanmpope@gmail.com>
2022-08-30 15:58:06 -0600

    test: add merchant find edge case API test

--- spec/requests/api/v1/merchants_spec.rb
+++ spec/requests/api/v1/merchants_spec.rb
@@ -230,5 +230,21 @@ describe "Merchants API" do
             expect(response.status).to eq(400) 
         end 
 
+        it "returns an empty array when no items match the search - find API" do
+            merchant1 = Merchant.create!(name: "K-MART", created_at: Time.now, updated_at: Time.now)
+            merchant2 = Merchant.create!(name: "Cheese Mart", created_at: Time.now, updated_at: Time.now)
+            merchant3 = Merchant.create!(name: "Bob's Shoes", created_at: Time.now, updated_at: Time.now)
+            merchant4 = Merchant.create!(name: "Adventure Shop", created_at: Time.now, updated_at: Time.now)
+
+            get "/api/v1/merchants/find?name=abcdefghij"
+            
+            expect(response).to be_successful
+            
+            merchants = JSON.parse(response.body, symbolize_names: true)
+
+            expect(merchants).to have_key(:data)
+            expect(merchants[:data].count).to eq(0) 
+         end
+
     end 
 end
