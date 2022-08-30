c30917c13b6c842c653c8f7518773ec69b6420dd
Jonathan Pope <jonathanmpope@gmail.com>
2022-08-30 15:48:36 -0600

    test: model test for merchant find all by name method

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -9,4 +9,19 @@ RSpec.describe Merchant, type: :model do
     it { should validate_presence_of :name }
   end
 
+  describe 'model tests' do
+    it "#self.find_by_name(name)" do
+      merchant1 = Merchant.create!(name: "K-MART", created_at: Time.now, updated_at: Time.now)
+      merchant2 = Merchant.create!(name: "Cheese Mart", created_at: Time.now, updated_at: Time.now)
+      merchant3 = Merchant.create!(name: "Bob's Shoes", created_at: Time.now, updated_at: Time.now)
+      merchant4 = Merchant.create!(name: "Adventure Shop", created_at: Time.now, updated_at: Time.now)
+
+      merchants = Merchant.find_by_name("mart")
+      data = JSON.parse(merchants.to_json, symbolize_names: true)
+
+      expect(data[:data].count).to eq(2)
+      expect(data[:data][0][:attributes][:name]).to eq("K-MART")
+      expect(data[:data][1][:attributes][:name]).to eq("Cheese Mart")
+    end 
+  end
 end 
